738015a147ab47bba7387ebaf7e7efcdb7595c1f
Carl Hurles <carlhurles28@gmail.com>
2026-07-05 11:26:52 +0100

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'uniform-ex-inventory-management',
+  webDir: 'dist'
+};
+
+export default config;
