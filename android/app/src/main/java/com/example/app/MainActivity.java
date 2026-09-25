8b3613767f25910946a7e48fafc7f5d49cfcd270
Carl Hurles <carlhurles28@gmail.com>
2026-09-25 12:16:34 +0100

    try to fix

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.uniformexchange.inventory;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.uniformexchange.inventory',
   appName: 'uniform-ex-inventory-management',
   webDir: 'dist'
 };
